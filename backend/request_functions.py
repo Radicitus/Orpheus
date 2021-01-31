c88cd098cbee140e7af6595392c1c862376bfed4
david5010 <david5010@icloud.com>
2021-01-31 06:17:46 -0500

    Added get_recommendations Added a route to recs that shows a playlist

--- backend/request_functions.py
+++ backend/request_functions.py
@@ -26,14 +26,14 @@ USER_READ_PRIVATE = "user-read-private"
     Helper
 '''
 
-def get_personalized_data(auth_token, mode, option):
-    auth_header = spotify_user_auth.authorize(auth_token)
+def get_personalized_data(auth_header, mode, option):
+    # auth_header = spotify_user_auth.authorize(auth_token)
     url = ''.join([SPOTIFY_API_URL,f'me/top/{option}/'])
     response = requests.get(url, headers=auth_header)
     return response.json()
 
 '''
-    Get top Tracks
+    Get top Tracks - 2 tracks
 '''
 
 def get_top_tracks_id(auth_token,mode):
@@ -41,9 +41,11 @@ def get_top_tracks_id(auth_token,mode):
     all_track_id = []
     for i in track_id_data:
         all_track_id.append(i['id'])
-    return all_track_id
+    print(all_track_id)
+    return all_track_id[:2]
+
 '''
-    Get top Artists
+    Get top Artists - just one
 '''
 
 def get_top_artist_id(auth_token,mode):
@@ -51,10 +53,11 @@ def get_top_artist_id(auth_token,mode):
     all_artist_id = []
     for i in artist_id_data['items']:
         all_artist_id.append(i['id'])
-    return all_artist_id
+    print(all_artist_id)
+    return all_artist_id[0]
 
 '''
-    Get Top Genres
+    Get Top Genres - 2 genres
 '''
 
 def get_top_genres(auth_token,mode):
@@ -64,24 +67,25 @@ def get_top_genres(auth_token,mode):
         all_genres.extend(i['genres'])
     genre_set = set(all_genres)
     top_5 = sorted(genre_set, key=lambda x: all_genres.count(x), reverse=True)[0:5]
-    return top_5
+    print(top_5)
+    return top_5[3:]
 
 '''
     Get Recommendations
 '''
 
-def get_recommendations(auth_header, mode, genre_list):
-
+def get_recommendations(auth_header, mode, artist_list, genre_list, track_list):
+    
     auth_header = auth_header
     genre_string = ','.join(genre_list)
-    genre_param = quote(genre_string)
+    track_list = ','.join(track_list)
 
-    artist_url = urlencode({'seed_artists': '7ENzCHnmJUr20nUjoZ0zZ1'})
-    genre_url = urlencode({'seed_genres': genre_param})
-    track_url = urlencode({'seed_tracks': '0rFHElzeddB9ymDjgpBENX'})
+    artist_url = urlencode({'seed_artists': artist_list})
+    genre_url = urlencode({'seed_genres': genre_string})
+    track_url = urlencode({'seed_tracks': track_list})
     tempo_url = urlencode(TARGET_TEMPO) #update this at some point to incorporate mode
     limit_url = urlencode(REC_LIMIT)
-    query = '&'.join([limit_url, artist_url, genre_url, track_url,tempo_url])
+    query = '&'.join([limit_url, artist_url, genre_url, track_url, tempo_url])
     url = ''.join([SPOTIFY_API_URL, REC_URL_FRAG, query])
     print(url)
     print(auth_header)
@@ -89,6 +93,12 @@ def get_recommendations(auth_header, mode, genre_list):
     print(response.json())
     return response.json()
 
+'''
+    Handle everything
+'''
+
+def handle_everything():
+    pass
 
 '''
     GET - Get User Information
@@ -125,6 +135,8 @@ def create_playlist(auth_header, user_id):
 def add_to_playlist(auth_header, rec_list, playlist_id):
     auth_header = spotify_user_auth.authorize(auth_header)
 
-
+'''
+    
+'''
 
 
