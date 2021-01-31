588a825723a0c28231f872f08521bb04516915ce
IanC <ian.marc.c@gmail.com>
2021-01-31 04:20:49 -0800

    removed old test templates and associated app.routes

--- app.py
+++ app.py
@@ -25,58 +25,6 @@ def callback():
     return redirect(url_for('index'))
 
 
-@app.route('/top/tracks/<mode>')
-def top_tracks(mode):
-    track_data = request_functions.get_top_tracks_id(session['auth_header'], mode)
-    return
-@app.route('/top/genres/<mode>')
-def top_genres(mode):
-    genre_data = request_functions.get_top_genres(session['auth_header'], mode)
-    # artist_data = request_functions.get_top_artist_id(session['user_auth'], mode)
-    return genre_data
-
-@app.route('/top/artists/<mode>')
-def top_artists(mode):
-    top_artists = request_functions.get_top_artist_id(session['auth_header'], mode)
-    return top_artists
-
-@app.route('/recs/<mode>')
-def hardcode_get_recs(mode):
-    top_tracks = request_functions.get_top_tracks_id(session['auth_header'], mode)
-    top_artists = request_functions.get_top_artist_id(session['auth_header'], mode)
-    top_genres = request_functions.get_top_genres(session['auth_header'], mode)
-
-    print(f'showing top_artists {top_artists}')
-    print(f'showing top tracks{top_tracks}')
-    print(f'showing top genres {top_genres}')
-
-    recs_data = request_functions.get_recommendations(session['auth_header'], mode, top_artists, top_genres, top_tracks)
-    return render_template('rec_list_test.html', recs=recs_data['tracks'])
-
-
-@app.route('/createPlaylist/', methods=['POST', 'GET'])
-def create_playlist():
-    #user_data = request_functions.get_user_info(session['user_auth'])
-    print('createPlaylist')
-    playlist_data = request_functions.create_playlist(session['auth_header'], session['user_data']['id'])
-    session['playlist_data'] = playlist_data.json()
-    return str(playlist_data.json())
-
-@app.route('/addToPlaylist/', methods=['POST', 'GET'])
-def add_to_playlist():
-    print('addToPlaylist')
-    playlist_data = request_functions.add_to_playlist(session['auth_header'],
-                                                      session['playlist_data'],
-                                                      session['recs_data'])
-    return "added songs hell yeah"
-
-@app.route('/user/')
-def get_user():
-    user_data = request_functions.get_user_info(session['auth_header'])
-    session['user_data'] = user_data
-    return render_template('get_user_test.html', user=user_data)
-
-
 @app.route('/complete/<mode>')
 def complete_playlist(mode):
     complete_playlist_data = request_functions.get_complete_playlist(session['auth_header'], mode)
