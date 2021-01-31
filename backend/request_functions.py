ed02d0fd7c45b4139e5b9926859488bd565629c2
IanC <ian.marc.c@gmail.com>
2021-01-30 17:19:55 -0800

    added constants and import statements to request_functions.py

---
+++ backend/request_functions.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+API_VERSION = "v1/"
+SPOTIFY_API_URL = "https://api.spotify.com/"+API_VERSION
+
+with open("./credentials/credentials.txt", "r") as f:
+    credentials = json.load(f)
+
+SPOTIFY_AUTH_BASE_URL = "https://accounts.spotify.com/{}"
+SPOTIFY_AUTH_URL = SPOTIFY_AUTH_BASE_URL.format('authorize')
+SPOTIFY_TOKEN_URL = SPOTIFY_AUTH_BASE_URL.format('api/token')
