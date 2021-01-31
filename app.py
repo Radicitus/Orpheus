ec9778e9ee936664a431ab4107cb43a622e5a738
Radicitus <novivolatus@gmail.com>
2021-01-31 06:54:35 -0800

    Added playlist_creation route (to be completed by Ian in request_functions)
    Signed-off-by: Radicitus <novivolatus@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, redirect, session, url_for
 import json
 import os
-from backend import spotify_user_auth
+
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_cors import CORS
+
 from backend import request_functions
+from backend import spotify_user_auth
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -22,21 +26,22 @@ def callback():
     auth_token = request.args['code']
     session['user_auth'] = auth_token
     session['auth_header'] = spotify_user_auth.authorize(auth_token)
-    return redirect(url_for('index'))
+    return redirect(url_for('playlist_creation'))
 
 
 @app.route('/selections', methods=['POST'])
 def save_selections():
     response = request.form
-    print(response['pumped'])
-    return 'OK'
+    session['selections'] = response
+    return redirect(url_for('auth'))
 
-  
-@app.route('/complete/<mode>')
-def complete_playlist(mode):
-    complete_playlist_data = request_functions.get_complete_playlist(session['auth_header'], mode)
-    print(complete_playlist_data)
-    return render_template('complete_test.html', playlist=complete_playlist_data)
+
+@app.route('/playlist')
+def playlist_creation():
+    pump_level = session['selections']['pumped']
+    bpm = session['selections']['bpm']
+    complete_playlist_data = request_functions.get_complete_playlist(session['auth_header'], [pump_level, bpm])
+    return render_template('final.html')
 
 
 if __name__ == '__main__':
